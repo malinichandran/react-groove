0aa25fee9e8cc9eec69a208b2d540229ce1cedb7
malini chandran <malinichandran@Malinis-MacBook-Air.local>
2021-08-13 11:25:09 -0700

    filtered videos without id

--- client/src/search/VideoList.js
+++ client/src/search/VideoList.js
@@ -4,15 +4,25 @@ import "./video.css";
 import ListOfPlaylists from "../playlists/ListOfPlaylists";
 
 const VideoList = ({videos , handleVideoSelect}) => {
-    const renderedVideos =  videos.map((video) => {
-        return(
-        <div>
+    // const renderedVideos =  videos.map((video) => {
+    //     return(
+    //     <div>
             
-        <VideoItem className="video-list" key={video.id.videoId} video={video} handleVideoSelect={handleVideoSelect} />
+    //     <VideoItem className="video-list" key={video.id.videoId} video={video} handleVideoSelect={handleVideoSelect} />
        
-        </div>
-    )});
-
+    //     </div>
+    // )});
+    const renderedVideos = videos.map((video) => {
+        if(video.id.videoId === undefined){
+            return;
+        }else{
+            return(
+                <div>
+                    <VideoItem className="video-list" key={video.id.videoId} video={video} handleVideoSelect={handleVideoSelect} />
+                </div>
+            )
+        }
+    })
     return <div >{renderedVideos}</div>;
 };
 export default VideoList;
